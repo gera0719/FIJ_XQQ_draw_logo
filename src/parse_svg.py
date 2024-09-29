1dec737b491dd08ee581ad096b8ea5c8e221cee6
gera0719 <gergonovak503@gmail.com>
2024-09-29 18:39:57 +0200

    extracting paths one-by-one and handling bezier curves

--- src/parse_svg.py
+++ src/parse_svg.py
@@ -12,11 +12,60 @@ def extractSvgPaths(filePath):
     allPathsData = [path.get('d') for path in pathElements if path.get('d')]
     return allPathsData
 
+#
+#extracting paths one-by-one from the allPathsData list
+def extractCoordinates(pathData):
+    points = []
+    i = 0
+    while i < len(pathData):
+        if pathData[i] in 'ML':  
+            i += 1
+            x, y = '', ''
+            while pathData[i] != ',':
+                x += pathData[i]
+                i += 1
+            i += 1  
+            while i < len(pathData) and pathData[i] not in [' ', 'M', 'L', 'C']:
+                y += pathData[i]
+                i += 1
+            points.append((float(x), float(y)))
+        elif pathData[i] == 'C':  
+            i += 1
+            controlPoints = []
+            for _ in range(3):
+                x, y = '', ''
+                while pathData[i] != ',':
+                    x += pathData[i]
+                    i += 1
+                i += 1  # Skip comma
+                while i < len(pathData) and pathData[i] not in [' ', 'M', 'L', 'C']:
+                    y += pathData[i]
+                    i += 1
+                controlPoints.append((float(x), float(y)))
+            p0 = points[-1]  
+            points.extend(cubicBezierCurve(p0, controlPoints))
+        i += 1
+    return points
+#turtlesim cannot handle bezier curves, so we need to convert them to line segments
+def cubicBezierCurve(p0, controlPoints, numSteps=100):
+    p1, p2, p3 = controlPoints
+    points = []
+    for i in range(numSteps + 1):
+        t = i / numSteps
+        x = (1 - t)**3 * p0[0] + 3 * (1 - t)**2 * t * p1[0] + 3 * (1 - t) * t**2 * p2[0] + t**3 * p3[0]
+        y = (1 - t)**3 * p0[1] + 3 * (1 - t)**2 * t * p1[1] + 3 * (1 - t) * t**2 * p2[1] + t**3 * p3[1]
+        points.append((x, y))
+    return points
 
 
-svgFilePath = '/home/ajr/ros2_ws/src/nov_fij_draw_logo/img/sze1px10x10px.svg'
 
+svgFilePath = 'img/sze1px10x10px.svg'
 
 pathDataList = extractSvgPaths(svgFilePath)
 
-print(pathDataList)
+allCoordinates = []
+#printing the paths to check if the extraction was successful
+for pathData in pathDataList:
+    print("Path: ", pathData)
+    coordinates = extractCoordinates(pathData)
+    allCoordinates.extend(coordinates)
