cc587c05cce53b653461177ca36a10cb9e95bbe6
gera0719 <gergonovak503@gmail.com>
2024-10-12 19:36:05 +0200

    teleport function: changed teleportation to given coordinates, also doesn't change pen settings; contol turtle: almost finished the controls - note: only curves are left to do; constructor: initializes current_move counter, deleted curve vectors until unsure of them being okay, changed the timer settings

--- src/draw_logo.cpp
+++ src/draw_logo.cpp
@@ -1,10 +1,11 @@
 #include "rclcpp/rclcpp.hpp"
 #include "geometry_msgs/msg/twist.hpp"
-#include "turtlesim/msg/pose.hpp"
 #include "turtlesim/srv/set_pen.hpp"
 #include "turtlesim/srv/teleport_absolute.hpp"
 #include <chrono>
 #include <cmath>
+
+using namespace std::chrono_literals;
 
 class DrawLogo : public rclcpp::Node{
     private:
@@ -15,8 +16,7 @@ class DrawLogo : public rclcpp::Node{
     std::vector<std::pair<double, double>> vectors;
     std::vector<std::pair<double, double>> coordinates_t;
     rclcpp::Client<turtlesim::srv::TeleportAbsolute>::SharedPtr teleport_c;
-    
-
+    size_t current_move;
     
 
     void pen_on_off(bool on){
@@ -28,19 +28,19 @@ class DrawLogo : public rclcpp::Node{
         on_off->off = !on;
         pen_on_off_c->async_send_request(on_off, [this](rclcpp::Client<turtlesim::srv::SetPen>::SharedFuture)
         {
-            RCLCPP_INFO(this->get_logger(), "pen setting has been changed");
+            RCLCPP_INFO(this->get_logger(), "pen settings has been changed");
         });
     }
     void teleport(double x, double y){
-        pen_on_off(false);
+        
         auto teleport = std::make_shared<turtlesim::srv::TeleportAbsolute::Request>();
-        teleport->x = 1.0; 
-        teleport->y = 1.0;
+        teleport->x = x; 
+        teleport->y = y;
         teleport->theta = 0.0;
         teleport_c->async_send_request(teleport, [this](rclcpp::Client<turtlesim::srv::TeleportAbsolute>::SharedFuture)
         {
             RCLCPP_INFO(this->get_logger(), "teleportation has been completed");
-            pen_on_off(true);
+            
         });
     }
     
@@ -53,94 +53,70 @@ class DrawLogo : public rclcpp::Node{
             velocity_pub->publish(msg);
             rclcpp::sleep_for(std::chrono::milliseconds(100));
         }
-        // Stop the turtle
+        
         msg.linear.x = 0.0;
         velocity_pub->publish(msg);
     }
 
     void control_turtle(){
-        // 0.3 unit left
-        pen_on_off(true);
-        RCLCPP_INFO(this->get_logger(), "0.3 left, pen on");
-        move_turtle(0.3);
-
-        // pen off
-        RCLCPP_INFO(this->get_logger(), "pen off");
-        pen_on_off(false);
-        
-
-        
-        RCLCPP_INFO(this->get_logger(), "0.3 left, pen off");
-        move_turtle(0.3);
-
-        
-        RCLCPP_INFO(this->get_logger(), "pen on");
-        pen_on_off(true);
-        
-
-        
-        RCLCPP_INFO(this->get_logger(), "0.3 left, pen on");
-        move_turtle(0.3);
+        if(current_move < coordinates_t.size()){
+            pen_on_off(false);
+            teleport(coordinates_t[current_move].first, coordinates_t[current_move].second);
+            pen_on_off(true);
+            move_turtle(vectors[current_move].first, 1.0);
+            current_move++;
+            std::cout << "move: " << current_move << " of " << vectors.size() << std::endl;
+        }else{
+            rclcpp::shutdown();
+        }
     }
-    
+
     public:
 
-    DrawLogo() : Node("draw_logo"){
+    DrawLogo() : Node("draw_logo"), current_move(0){
         
         velocity_pub = this->create_publisher<geometry_msgs::msg::Twist>("turtle1/cmd_vel", 10);
         pen_on_off_c = this->create_client<turtlesim::srv::SetPen>("turtle1/set_pen");
         teleport_c = this->create_client<turtlesim::srv::TeleportAbsolute>("/turtle1/teleport_absolute");
-
-        //the easiest way to control the turtle will be, if we'd stored the vectors of the movement in a vector
-        
-
+    
         vectors = {
             //lines -> {x, y of vector}
-            /*teleport to starting point (-4.0, -3.0) */{5.7, 0.0},
-            /*teleport to (-2.5, -2.25)*/{4.2, 0.0},
-            /*teleport to (-4.0, -1.5)*/{5.7, 0.0},
+            {5.7, 0.0},
+            {4.2, 0.0},
+            {5.7, 0.0},
             //lower three long lines done
-            /*teleport to (2.5, -3.0)*/{1.5, 0.0},
-            /*teleport to (2.5, -2.25)*/{1.5, 0.0},
-            /*teleport to (2.5, -1.5)*/{1.5, 0.0},
-            /*teleport to (2.5, -0.75)*/{1.5, 0.0},
-            /*teleport to (2.5, 0.0)*/{1.5, 0.0},
-            /*teleport to (2.5, 0.75)*/{1.5, 0.0},
-            /*teleport to (2.5, 1.5)*/{1.5, 0.0},
-            /*teleport to (2.5, 2.25)*/{1.5, 0.0},
-            /*teleport to (2.5, 3.0)*/{1.5, 0.0},
+            {1.5, 0.0},
+            {1.5, 0.0},
+            {1.5, 0.0},
+            {1.5, 0.0},
+            {1.5, 0.0},
+            {1.5, 0.0},
+            {1.5, 0.0},
+            {1.5, 0.0},
+            {1.5, 0.0},
             //*9 short lines done
 
-            /*teleport to (-2.5, -0.75)*/{5.6569, 0,/*i don't know that yet :C*/},
-            /*teleport to (-2.5, 0.0)*/{4.5962, 0}
-
+            //only the curves are left to do
         };
-        //storing the coordinates for teleporting in a vector
+        //teleport coords
         coordinates_t = {
-            {-4.0, -3.0},
-            {-2.5, -2.25},
-            {-4.0, -1.5},
-
-            {2.5, -3.0},
-            {2.5, -2.25},
-            {2.5, -1.5},
-            {2.5, -0.75},
-            {2.5, 0.0},
-            {2.5, 0.75},
-            {2.5, 1.5},
-            {2.5, 2.25},
-            {2.5, 3.0},
-
-            {-2.5, -0.75},
-            {-2.5, 0.0}
-
+            {1.0, 2.0},
+            {2.5, 2.75},
+            {1.0, 3.5},
+            {7.5, 2.0},
+            {7.5, 2.75},
+            {7.5, 3.5},
+            {7.5, 4.25},
+            {7.5, 5.0},
+            {7.5, 5.75},
+            {7.5, 6.5},
+            {7.5, 7.25},
+            {7.5, 8.0},
+            //only the curves are left to do
         };
-
-
-        // cooldown after init
+        //continus impulse
         timer = this->create_wall_timer(
-            std::chrono::seconds(1), std::bind(&DrawLogo::control_turtle, this));
-
+            1.25s, std::bind(&DrawLogo::control_turtle, this));
     };
 };
 
